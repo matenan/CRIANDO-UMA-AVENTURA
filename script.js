a538b4253536145919329c6b41bebe1a20b0fd17
matenan <maria.tenan@escola.pr.gov.br>
2024-11-14 10:58:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-const avanca = document.querySelectorAll(' .btn-proximo')
+const avanca = document.querySelectorAll('.btn-proximo');
 
-avanca.forEach(button => { 
+avanca.forEach(button => {
     button.addEventListener('click', function(){
-     const atual = document.querySelector('.ativo');
-     const proximopasso = 'passo-' + this.getAttribute('data-proximo');
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-     atual.classList.remove('ativo');
-     document.getElementById(proximopasso).classList.add('ativo');
-     
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
     })
 })
